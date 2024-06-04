794a2df7a56ac5203c3739583bf801b06b42f313
Ayush Tamang <ayutmg456@gmail.com>
2024-06-04 17:22:26 +0545

    arrayans

---
+++ javascript/string/level1.js
@@ -0,0 +1,20 @@
+//Q1 find list of numbers divisible by 3
+
+const arr = [3,5,1,2,9,12]
+const ansarr = arr.filter((item,id)=>{
+    if(item%3 === 0 ){
+    return item;
+    }
+})
+console.log(ansarr)
+
+//expected output---> [3,9,12]
+// hint: %3 === 0
+// filter
+//syntax:
+// arr.filter((item,id)=>{
+//     //
+// })
+
+
+//----------------
